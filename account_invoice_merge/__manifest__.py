7aeede47ff395447917d22309702c09bb09d29d9
vinclar <albert@lightware.es>
2018-07-18 16:29:15 +0200

    Update __manifest__.py

--- account_invoice_merge/__manifest__.py
+++ account_invoice_merge/__manifest__.py
@@ -7,11 +7,11 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Account Invoice Merge',
+    'name': 'Account Purchase Merge',
     'version': '10.0.1.0.0',
-    'category': 'Finance',
-    'summary': "Merge invoices in draft",
-    'author': "Elico Corp,Odoo Community Association (OCA)",
+    'category': 'Purchase',
+    'summary': "Merge purchases in draft",
+    'author': "doo Community Association (OCA)",
     'website': 'http://www.openerp.net.cn',
     'license': 'AGPL-3',
     'depends': ['account'],
